c7f45a2f82494a7fcfcf8a0b00d518f0500f149f
Anant Goel <anantdgoel@gmail.com>
2017-02-06 15:10:37 -0500

    fixed vue router

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import Home from './components/Home.vue'
+// import Home from './components/Home.vue'
 import Signup from './components/Signup.vue'
 import Login from './components/Login.vue'
 import VueRouter from 'vue-router'
@@ -11,29 +11,22 @@ import VueResource from 'vue-resource'
 Vue.use(VueResource)
 Vue.use(VueRouter)
 
-const router = new VueRouter()
+const routes = [
+  {path: '/', component: App},
+  {path: '/signup', component: Signup},
+  {path: '/login', component: Login}
+]
 
-router.map({
-  '/home': {
-    component: Home
-  },
-  '/signup': {
-    component: Signup
-  },
-  '/login': {
-    component: Login
-  }
-})
-
-// Redirect to the home route if any routes are unmatched
-router.redirect({
-  '*': '/home'
-})
-
-// Start the app on the #app div
-router.start(App, '#app')
+const router = new VueRouter({routes})
 
 /* eslint-disable no-new */
+new Vue({
+  router
+}).$mount('#app')
+
+// Start the app on the #app div
+// router.start(App, '#app')
+//
 // new Vue({
 //   el: '#app',
 //   template: '<App/>',
