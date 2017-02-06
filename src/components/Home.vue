ef5b31e4eced106151a24eb149db678c7d3dfede
Mihir Tiwari <mtiw999@gmail.com>
2017-02-06 14:37:33 -0500

    started adding template stuff and vue-router is dumb

---
+++ src/components/Home.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="home">
+        <a class="btn btn-warning btn-lg" href="#" role="button">Sign Up</a>
+        <a class="btn btn-warning btn-lg" href="#" role="button">Login</a>
+    </div>
+</template>
+
+<style>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -2,10 +2,40 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import Home from './components/Home.vue'
+import Signup from './components/Signup.vue'
+import Login from './components/Login.vue'
+import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
+
+Vue.use(VueResource)
+Vue.use(VueRouter)
+
+const router = new VueRouter()
+
+router.map({
+  '/home': {
+    component: Home
+  },
+  '/signup': {
+    component: Signup
+  },
+  '/login': {
+    component: Login
+  }
+})
+
+// Redirect to the home route if any routes are unmatched
+router.redirect({
+  '*': '/home'
+})
+
+// Start the app on the #app div
+router.start(App, '#app')
 
 /* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  template: '<App/>',
-  components: { App }
-})
+// new Vue({
+//   el: '#app',
+//   template: '<App/>',
+//   components: { App }
+// })
