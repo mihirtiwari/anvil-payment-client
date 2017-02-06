e0c7563a23ae2ea86eb85950edf565bc72e503bf
Mihir Tiwari <mtiw999@gmail.com>
2017-02-06 14:54:17 -0500

    vue-router issues

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,10 +1,16 @@
 <template>
-    <div class="home">
+    <div class="home" id="buttons">
         <a class="btn btn-warning btn-lg" href="#" role="button">Sign Up</a>
         <a class="btn btn-warning btn-lg" href="#" role="button">Login</a>
     </div>
 </template>
 
 <style>
+a {
+    width: 10%;
+}
 
+#buttons {
+    margin-top: 100px;
+}
 </style>
