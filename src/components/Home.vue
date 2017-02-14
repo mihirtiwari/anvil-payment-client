39f57843357bc266c62023b0b9689a29c71aef72
Mihir Tiwari <mtiw999@gmail.com>
2017-02-13 20:45:59 -0500

    added notice on home page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,20 @@
 <template>
     <div id="home">
+        <div class="container">
+        <div class="col-md-6 col-md-offset-3" id="message">
+            <p>
+                Welcome to the Anvil Payment System! Sign up today and pay only $5
+                on your first month (that's 50% off!) at The Anvil! After your
+                first month, you will be charged $10 per month, starting mid March.
+            </p>
+            <br>
+            <p>
+                <b>NOTE:</b> If you sign up, you will have to input your credit card number
+                so proceed with caution
+            </p>
+        </div>
+        </div>
+        <div class="container">
         <div id="buttons" class="col-md-offset-3 col-md-6">
             <p>
                 <button type="button" class="btn btn-warning link" v-on:click='tosignup'>
@@ -11,6 +26,7 @@
                         Login
                 </button>
             </p>
+        </div>
         </div>
         <router-view></router-view>
     </div>
@@ -39,7 +55,7 @@ export default {
 }
 
 .btn:hover {
-    background-color: #FFCD2E; 
+    background-color: #FFCD2E;
 }
 
 button {
@@ -47,7 +63,7 @@ button {
 }
 
 #buttons {
-    margin-top: 150px;
+    margin-top: 50px;
     text-align: center;
 }
 
@@ -57,5 +73,14 @@ button {
 
 .link {
     font-size: 30px;
+}
+
+#message {
+    margin-top: 50px;
+}
+
+#message p {
+    text-align: center;
+    font-size: 20px;
 }
 </style>
